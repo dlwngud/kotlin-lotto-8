4d18fffd94789c6262fd7b6b41bccdab7e4a71bb
이주형 <leewngud980411@gmail.com>
2025-11-03 15:48:33 +0900

    feat: 당첨 번호, 보너스 번호 유효성 검사

--- src/main/kotlin/lotto/InputValidator.kt
+++ src/main/kotlin/lotto/InputValidator.kt
@@ -7,6 +7,18 @@ class InputValidator {
         if (amount % PRICE_PER_LOTTO != 0) throw IllegalArgumentException(ErrorMessage.INVALID_AMOUNT.message)
     }
 
+    fun validateWinningNumbers(winningNumbers: List<Int>) {
+        if (winningNumbers.size != 6) throw IllegalArgumentException(ErrorMessage.SIX_NUMBER.message)
+        if (winningNumbers.any { it !in 1..45 }) throw IllegalArgumentException(ErrorMessage.INVALID_NUMBER.message)
+        if (winningNumbers.size != winningNumbers.toSet().size) throw IllegalArgumentException(ErrorMessage.DUPLICATE_NUMBER.message)
+    }
+
+    fun validateBonusNumber(bonusNumber: Int?, winningNumbers: List<Int>) {
+        if (bonusNumber == null) throw IllegalArgumentException(ErrorMessage.INVALID_INPUT.message)
+        if (bonusNumber !in 1..45) throw IllegalArgumentException(ErrorMessage.INVALID_NUMBER.message)
+        if (winningNumbers.contains(bonusNumber)) throw IllegalArgumentException(ErrorMessage.BONUS_DUPLICATE.message)
+    }
+
     companion object {
         const val PRICE_PER_LOTTO = 1_000
     }
