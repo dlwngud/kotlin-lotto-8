4977715b3b510544bba7c6a543a33d1d128ea2a8
이주형 <leewngud980411@gmail.com>
2025-11-03 20:07:21 +0900

    test: 로또 Class 테스트

--- src/test/kotlin/lotto/LottoTest.kt
+++ src/test/kotlin/lotto/LottoTest.kt
@@ -2,6 +2,7 @@ package lotto
 
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.assertThrows
+import org.assertj.core.api.Assertions.assertThat
 
 class LottoTest {
     @Test
@@ -19,5 +20,33 @@ class LottoTest {
         }
     }
 
-    // TODO: 추가 기능 구현에 따른 테스트 코드 작성
+    @Test
+    fun `로또 번호에 1~45 범위를 벗어나는 숫자가 있으면 예외가 발생한다`() {
+        assertThrows<IllegalArgumentException> {
+            Lotto(listOf(1, 2, 3, 4, 5, 0))
+        }
+        assertThrows<IllegalArgumentException> {
+            Lotto(listOf(1, 2, 3, 4, 5, 46))
+        }
+    }
+
+    @Test
+    fun `정상적인 6개의 서로 다른 1~45 번호로 Lotto 생성 가능`() {
+        val lotto = Lotto(listOf(6, 5, 4, 3, 2, 1))
+        assertThat(lotto.sortedNumbers()).containsExactly(1, 2, 3, 4, 5, 6)
+    }
+
+    @Test
+    fun `countMatch는 일치하는 번호 개수를 반환한다`() {
+        val lotto = Lotto(listOf(1, 2, 3, 4, 5, 6))
+        val winning = listOf(1, 2, 3, 10, 11, 12)
+        assertThat(lotto.countMatch(winning)).isEqualTo(3)
+    }
+
+    @Test
+    fun `contains는 보너스 번호 포함 여부를 확인한다`() {
+        val lotto = Lotto(listOf(1, 2, 3, 4, 5, 6))
+        assertThat(lotto.contains(3)).isTrue()
+        assertThat(lotto.contains(7)).isFalse()
+    }
 }
