70827a2aa78013dd016834f6adf6ca61f5ccc951
이주형 <leewngud980411@gmail.com>
2025-11-03 14:48:21 +0900

    feat: 로또 구입 금액 입력 유효성 검사

---
+++ src/main/kotlin/lotto/InputValidator.kt
@@ -0,0 +1,12 @@
+package lotto
+
+class InputValidator {
+    fun validatePurchaseAmount(amount: Int) {
+        if (amount <= 0) throw IllegalArgumentException(ErrorMessage.NEGATIVE_AMOUNT.message)
+        if (amount % PRICE_PER_LOTTO != 0) throw IllegalArgumentException(ErrorMessage.INVALID_AMOUNT.message)
+    }
+
+    companion object {
+        const val PRICE_PER_LOTTO = 1_000
+    }
+}
