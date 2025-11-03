8dcf7c9c5cf05089d27f4c22a958e1ac54127b7f
이주형 <leewngud980411@gmail.com>
2025-11-03 15:48:18 +0900

    feat: 당첨 번호, 보너스 번호 입력

--- src/main/kotlin/lotto/InputHandler.kt
+++ src/main/kotlin/lotto/InputHandler.kt
@@ -10,4 +10,17 @@ class InputHandler {
         inputValidator.validatePurchaseAmount(purchaseAmount)
         return purchaseAmount!! / InputValidator.PRICE_PER_LOTTO
     }
+
+    fun readWinningNumbers(): List<Int> {
+        val input = Console.readLine()
+        val winningNumbers = input.split(",").map { it.trim().toInt() }
+        inputValidator.validateWinningNumbers(winningNumbers)
+        return winningNumbers
+    }
+
+    fun readBonusNumber(winningNumbers: List<Int>): Int {
+        val bonusNumber = Console.readLine().toIntOrNull()
+        inputValidator.validateBonusNumber(bonusNumber, winningNumbers)
+        return bonusNumber!!
+    }
 }
